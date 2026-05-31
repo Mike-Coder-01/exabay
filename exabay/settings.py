3bec27fe2e8c3bcc9189b3e21fe991c6db947358
Mike-Coder-01 <michaelmsita95@gmail.com>
2026-05-31 11:49:12 +0300

    Sending email after register as seller1

--- exabay/settings.py
+++ exabay/settings.py
@@ -12,7 +12,7 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(
-    DEBUG=(bool, False)
+    DEBUG=(bool, True)
 )
 
 environ.Env.read_env(BASE_DIR / ".env")

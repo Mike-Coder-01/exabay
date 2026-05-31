7878d798f6f225ba3e7273851a250303b36274af
Mike-Coder-01 <michaelmsita95@gmail.com>
2026-05-31 11:56:55 +0300

    Content-Type removed in sending email

--- main/utils.py
+++ main/utils.py
@@ -4,12 +4,11 @@ from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 
 def send_custom_email(user_email, username, subject, body):
-    # Convert plain text line breaks to HTML before passing to template
     body_html = body.replace("\n", "<br>")
 
     html_content = render_to_string("account/email/send_email.html", {
         "username": username,
-        "body": body_html,  # ✅ HTML line breaks
+        "body": body_html,
     })
 
     email = EmailMultiAlternatives(
@@ -17,7 +16,7 @@ def send_custom_email(user_email, username, subject, body):
         body=strip_tags(html_content),
         from_email=settings.DEFAULT_FROM_EMAIL,
         to=[user_email],
-        headers={"Content-Type": "text/plain; charset=utf-8"},  # ✅ explicit UTF-8
+        # ✅ no headers= here
     )
 
     email.attach_alternative(html_content, "text/html")

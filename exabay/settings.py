839977aeb9250912c1101aed81a3d537e0270dff
Mike-Coder-01 <michaelmsita95@gmail.com>
2026-08-13 21:45:08 +0300

    Testing debug on on files display

--- exabay/settings.py
+++ exabay/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(
-    DEBUG=(bool, False)
+    DEBUG=(bool, True)
 )
 
 environ.Env.read_env(BASE_DIR / ".env")
